c3a0699ab915f58141b6d6cdb36c72ec4f101f81
FrenkenFlores <fflores@student.21-school.ru>
2020-11-14 00:09:23 +0300

    update #6: ft_read

--- ft_read.s
+++ ft_read.s
@@ -1,16 +1,16 @@
 			extern	___error
-			global	_ft_write
+			global	_ft_read
 			section	.bss
 			section	.data
 			section	.text
-_ft_write:  mov		rax, 0x2000004
+_ft_read:	mov		rax, 0x2000003
 			syscall
 			cmp		edi, 0
 			jl		.error
 			cmp		esi, 0x0
 			je		.error
 			cmp		edx, 0
-			jl		.error		
+			jl		.error
 			ret
 
 .error:		push	rdx
--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
 
 void		ft_putchar(const char ch);
 ssize_t		ft_write(int fd, const void *buf, size_t n);
+ssize_t		ft_read(int fd, void *buf, size_t n);
 size_t		ft_strlen(const char *s);
 
 int        main(int argc, char **argv)
@@ -11,9 +14,13 @@ int        main(int argc, char **argv)
 	char c = 'A';
 	void *v = NULL;
 	int a = 0;
+	char	str[10];
 //	ft_putchar(c);
-//	a = ft_write(1, "hellodsadsdsd", 8);
-	a = ft_strlen("dsads");
-	printf ("\n%i\n", a);
+//	a = ft_write(1, "hellodsadsdsd", -8);
+//	a = ft_strlen("dsads");
+//	int fd = open("./material.txt", O_RDONLY);
+//	a = read(fd, str, -1);
+	printf ("\n%i\n%s", a, str);
+	printf("\nerr = %i\n", errno);
 	return (0);
 }
