be0f296fae46b8d4bae5861166797187ca729152
FrenkenFlores <fflores@student.21-school.ru>
2020-11-13 21:04:27 +0300

    update #5: ft_strlen

---
+++ ft_strlen.s
@@ -0,0 +1,9 @@
+			global  	_ft_strlen
+			section 	.bss
+			section		.data
+			section		.text
+_ft_strlen:	mov			rax, -1
+.loop:		inc			rax
+			cmp			byte [rdi+rax], 0
+			jne			.loop
+			ret
--- main.c
+++ main.c
@@ -1,8 +1,19 @@
-void	ft_putchar(const char ch);
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
 
-int		main(int argc, char **argv)
+void		ft_putchar(const char ch);
+ssize_t		ft_write(int fd, const void *buf, size_t n);
+size_t		ft_strlen(const char *s);
+
+int        main(int argc, char **argv)
 {
 	char c = 'A';
-	ft_putchar(c);
+	void *v = NULL;
+	int a = 0;
+//	ft_putchar(c);
+//	a = ft_write(1, "hellodsadsdsd", 8);
+	a = ft_strlen("dsads");
+	printf ("\n%i\n", a);
 	return (0);
 }
